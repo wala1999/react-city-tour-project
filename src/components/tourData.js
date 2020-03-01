631f52149cdf3510487d0b31c18dfd48d61f784f
Wala  smirani <walasmirani@air-de-wala.home>
2020-03-01 12:43:09 +0100

    first commit

---
+++ src/components/tourData.js
@@ -0,0 +1,42 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "New York est une ville composée de 5 arrondissements à l'embouchure du fleuve Hudson et de l'océan Atlantique. En son centre se trouve Manhattan, un arrondissement densément peuplé faisant partie des principaux centres commerciaux, financiers et culturels du monde. Ses sites incontournables comprennent des gratte-ciel comme l'Empire State Building et l'immense Central Park. Le théâtre de Broadway est situé sur Times Square."
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "Paris, capitale de la France, est une grande ville européenne et un centre mondial de l'art, de la mode, de la gastronomie et de la culture. Son paysage urbain du XIXe siècle est traversé par de larges boulevards et la Seine. Outre les monuments comme la tour Eiffel et la cathédrale gothique Notre-Dame du XIIe siècle, la ville est réputée pour ses cafés et ses boutiques de luxe bordant la rue du Faubourg-Saint-Honoré."
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "Londres, la capitale de l'Angleterre et du Royaume-Uni, est une ville moderne dont l'histoire remonte à l'époque romaine. En son centre se dressent l'imposant Parlement, l'emblématique Big Ben et l'abbaye de Westminster, lieu de couronnement des monarques britanniques. De l'autre côté de la Tamise, le London Eye, la grande roue, offre une vue panoramique sur le South Bank Center, et toute la ville."
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "Capitale animée du Japon, Tokyo associe ultramoderne et traditionnel, gratte-ciels illuminés et temples historiques. L'opulent sanctuaire shinto Meiji est réputé pour son imposant portail et les bois qui l'entourent. Le palais impérial (ou Kōkyo) est installé dans de vastes jardins publics. Les nombreux musées de la ville proposent des expositions allant de l'art classique (musée national de Tokyo) à la reconstitution d'un théâtre kabuki (musée d'Edo-Tokyo)."
+  },
+  {
+    id: 5,
+    city: "Tunisie",
+    img: "./img/tunisie.jpg",
+    name: "tunisie paysage époustouflant",
+    info:
+      "La Tunisie est un pays d'Afrique du Nord situé sur la côte méditerranéenne et en bordure du désert du Sahara. Le musée national du Bardo à Tunis, la capitale, expose des pièces archéologiques allant des mosaïques romaines à l'art islamique. Le quartier de la Médina, avec son souk animé, encercle l'imposante mosquée Zitouna. À l'est, sur le site de l'ancienne cité de Carthage, se trouvent les thermes d'Antonin et d'autres ruines, et le musée national de Carthage et ses nombreux objets."
+  }
+];
